b984ea3b9534561ccbe4761fd9469cfcac640ca0
SilSecrect <silsecrect@gmail.com>
2023-09-13 14:49:32 -0300

    c0 - meu primeiro commit

---
+++ main.js
@@ -0,0 +1,37 @@
+// function verificar() {
+//     const nome = window.document.getElementById("fname")
+//     const sobre = window.document.getElementById("lname")
+//     const idade = window.document.getElementById("number")
+//     const texto = window.document.getElementById("texto")
+//     const resultado = (nome.value)
+//     const resultado1 = (sobre.value)
+//     const resultado2 = Number(idade.value)
+//     texto.innerHTML += `Seu nome é ${resultado} ${resultado1}, sua idade é ${resultado2} `
+
+//     if (resultado2 > 16) {
+//         texto.innerText += ` voce  pode votar`
+//     } else{
+//         texto.innerText += ` voce nao  pode votar`
+//     }
+  
+    
+// }
+
+function verificar() {
+    const nome = window.document.getElementById("fname")
+    const sobre = window.document.getElementById("lname")
+    const idade = window.document.getElementById("number")
+    const texto = window.document.getElementById("texto")
+    const resultado = (nome.value)
+    const resultado1 = (sobre.value)
+    const resultado2 = Number(idade.value)
+    
+
+    if (resultado2 > 16) {
+        alert(`Seu nome é ${resultado} ${resultado1}, sua idade é ${resultado2} VOCE PODE VOTAR`)
+    } else{
+        alert(`Seu nome é ${resultado} ${resultado1}, sua idade é ${resultado2}! VOCE NAO PODE VOTAR `)
+    }
+  
+    
+}
